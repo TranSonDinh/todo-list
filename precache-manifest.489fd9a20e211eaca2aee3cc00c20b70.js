49734dec10c0859400b687e72561f03a35efdde7
TranSonDinh <sondinh2703@gmail.com>
2020-11-08 21:55:08 +0700

    Updates

---
+++ precache-manifest.489fd9a20e211eaca2aee3cc00c20b70.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed22bee2429d5d1b5d082bab47acae5e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "bf4fc97b44619ff79269",
+    "url": "/todo-list/static/css/main.c1717deb.chunk.css"
+  },
+  {
+    "revision": "25020d4310a68d6ffacc",
+    "url": "/todo-list/static/js/2.0aa7fa11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-list/static/js/2.0aa7fa11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4fc97b44619ff79269",
+    "url": "/todo-list/static/js/main.cdbeef2b.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/todo-list/static/media/cancel.cad70081.svg"
+  },
+  {
+    "revision": "0d4e03c5d048be0276faddf7ad489009",
+    "url": "/todo-list/static/media/check-mark.0d4e03c5.svg"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/todo-list/static/media/check.8b4d6e74.svg"
+  },
+  {
+    "revision": "3a5afd7357c0a2c43162c69e03cf7d5a",
+    "url": "/todo-list/static/media/tick.3a5afd73.svg"
+  }
+]);
